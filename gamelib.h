2f7e1863fe5e42e425d254094b9c474d7bf61a71
marikaminni <73894636+marikaminni@users.noreply.github.com>
2020-12-10 12:35:09 +0100

    Create gamelib.h

---
+++ gamelib.h
@@ -0,0 +1,27 @@
+void menu(); //Dichiarazione del prototipo di funzione
+void imposta_gioco();
+void gioca();
+void termina_gioco();
+
+enum Stato_giocatore    {astronauta, impostore, assassinato, defenestrato};
+enum Nome_giocatore     {rosso,blu, verde, giallo, bianco, nero, marrone, arancione, viola, fucsia};
+enum Tipo_stanza        {vuota, quest_semplice, quest_complicata, botola};
+enum Chiamata_emergenza {effettuata, non_effettuata};
+
+struct Stanza
+{
+  struct Stanza* avanti;
+  struct Stanza* sinistra;
+  struct Stanza* destra;
+  struct Stanza* stanza_precedente;
+  enum Tipo_stanza descrizione;  
+  enum Chiamata_emergenza emergenza_chiamata;
+
+};
+
+struct Giocatore
+{
+  struct Stanza* posizione_stanza;
+  enum Stato_giocatore stato;
+  enum Nome_giocatore nome;
+};
