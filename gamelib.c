f0b80fc3c8eb78d52b3c2fd11f7d7346bf5fc469
marikaminni <73894636+marikaminni@users.noreply.github.com>
2020-12-10 12:37:21 +0100

    Create gamelib.c

---
+++ gamelib.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "gamelib.h"
+
+struct Giocatore* giocatori;
+struct Stanza* stanza_inizio;
+struct Stanza* lista_stanze;
+unsigned short quest_da_finire;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void menu() // Definizione della funzione
+{
+   printf("Menù di scelta:\n 1) Imposta gioco\n 2) Gioca\n 3) Termina gioco\n" );
+}
