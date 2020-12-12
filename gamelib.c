c84fa5be9c3e0989d846fd1fc6ad8459a89ecd12
marikaminni <73894636+marikaminni@users.noreply.github.com>
2020-12-12 13:04:58 +0100

    Update gamelib.c

--- gamelib.c
+++ gamelib.c
@@ -3,11 +3,24 @@
 #include "gamelib.h"
 
 struct Giocatore* giocatori;
-struct Stanza* stanza_inizio;
 struct Stanza* lista_stanze;
+struct Stanza* stanza_inizio;
 unsigned short quest_da_finire;
 
+void imposta_gioco()
+{
+  int scelta=0;
+  do
+  {
+    printf("Digitare un numero da 4 a 10 per impostare il numero dei giocatori:");
+    scanf("%d",&scelta );
+    if (scelta < 4 || scelta >10)
+    {
+      printf("si deve inserire un numero compreso fra 4 e 10,riprovare\n");
+    }
+  }while (scelta < 4 || scelta >10);
 
+}
 
 
 
