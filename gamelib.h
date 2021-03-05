f9ffda5d02850b70afe718ab3e8af68baaebcb50
marikaminni <73894636+marikaminni@users.noreply.github.com>
2021-03-05 21:56:36 +0100

    Update gamelib.h

--- gamelib.h
+++ gamelib.h
@@ -4,9 +4,9 @@ void gioca();
 void termina_gioco();
 
 enum Stato_giocatore    {astronauta, impostore, assassinato, defenestrato};
-enum Nome_giocatore     {rosso,blu, verde, giallo, bianco, nero, marrone, arancione, viola, fucsia};
+enum Nome_giocatore     {rosso, blu, verde, giallo, bianco, nero, marrone, arancione, viola, fucsia};
 enum Tipo_stanza        {vuota, quest_semplice, quest_complicata, botola};
-enum Chiamata_emergenza {effettuata, non_effettuata};
+enum Boolean            {effettuata, non_effettuata};
 
 struct Stanza
 {
@@ -14,8 +14,8 @@ struct Stanza
   struct Stanza* sinistra;
   struct Stanza* destra;
   struct Stanza* stanza_precedente;
-  enum Tipo_stanza descrizione;  
-  enum Chiamata_emergenza emergenza_chiamata;
+  enum Tipo_stanza descrizione;
+  enum Boolean emergenza_chiamata;
 
 };
 
