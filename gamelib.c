1a672d1597d40f82fa52c759682443de3ab5b30c
marikaminni <73894636+marikaminni@users.noreply.github.com>
2021-03-10 23:51:38 +0100

    creata la funzione prima stanza
    non funziona bene perché come stanza iniziale mi da sempre "stanza vuota"
    i nomi dei giocatori non si devono ripetere
    quando devo inserire le quest, se digito un carattere mi fa andare avanti con il gioco

--- gamelib.c
+++ gamelib.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -13,7 +12,9 @@ static void stampa_giocatori();
 static void inizia_gioco();
 static _Bool gioco_impostato=false;
 static int n_giocatori=0;
-
+const char* stato_giocatore[]= {"astronauta", "impostore", "assassinato", "defenestrato"};
+const char* tipo_stanza[]= {"vuota", "quest_semplice", "quest_complicata", "botola"};
+static void prima_stanza();
 void imposta_gioco()
 {
   time_t t;
@@ -23,7 +24,6 @@ void imposta_gioco()
   do
   {
     printf("Digitare un numero da 4 a 10 per impostare il numero dei giocatori: "); //richiedo il numero dei giocatori
-
     scanf("%d",&scelta );
     while(getchar() !='\n');
 
@@ -33,23 +33,24 @@ void imposta_gioco()
     }
   }while (scelta < 4 || scelta >10);
 
-  giocatori = (struct Giocatore*) malloc(sizeof(struct Giocatore)*scelta);
+  giocatori = (struct Giocatore*) malloc(sizeof(struct Giocatore)*scelta); //creo nell'heap l'array struct Giocatore* giocatori
+  stanza_inizio= (struct Stanza*) malloc(sizeof(struct Stanza));
   n_giocatori=scelta;
-  for (size_t i = 0; i < scelta; i++)
-  {
-    struct Giocatore *giocatore_corrente= malloc(sizeof(struct Giocatore));
-    giocatore_corrente->posizione_stanza= NULL;
-    giocatore_corrente->stato= astronauta;
-    giocatore_corrente->nome= rosso;
+  for (int i = 0; i < scelta; i++)
+    {
 
-    giocatori[i]= *giocatore_corrente;
-  }
+      struct Giocatore *giocatore_corrente= malloc(sizeof(struct Giocatore));
+      giocatore_corrente->posizione_stanza= NULL;
+      giocatore_corrente->nome= rand()%scelta; //genera i giocatori randomicamente
+      giocatore_corrente->stato= astronauta;
 
-  printf("Digitare il numero delle quest da eseguire per vincere il gioco: ");  // richiedo il numero delle quest necessarie per vincere il gioco
+      giocatori[i]= *giocatore_corrente;
 
+    }
+
+  printf("Digitare il numero delle quest da eseguire per vincere il gioco: ");
   scanf("%d", &n_quest);
   while(getchar() !='\n');
-
 
   printf("Decidere se:\n 1) Stampare i giocatori \n 2) Iniziare il gioco\n");
   scanf("%d", &scelta );
@@ -70,8 +71,6 @@ void imposta_gioco()
   gioco_impostato= true;
 }
 
-
-
 void gioca()
 {
   if (gioco_impostato==false) //non si può giocare se non si è impostato il gico
@@ -86,22 +85,75 @@ void gioca()
 
 void termina_gioco()
 {
-
     free(giocatori);
-
+    free(stanza_inizio);
 }
-
 
 void menu() // Definizione della funzione menu
 {
    printf("Menù di scelta:\n 1) Imposta gioco\n 2) Gioca\n 3) Termina gioco\n" );
 }
 
+void prima_stanza() // in questa funzione definisco il tipo della stanza iniziale
+{
+  stanza_inizio->avanti= NULL;
+  stanza_inizio->sinistra=NULL;
+  stanza_inizio->destra=NULL;
+  stanza_inizio->stanza_precedente=NULL;
+   int random= rand()%100;
+   if (random<=24)
+
+     printf("La stanza iniziale è di tipo %s\n", tipo_stanza[stanza_inizio->tipo] );
+
+   else
+
+     if (random>=25 && random<=39)
+     {
+       printf("La stanza iniziale è di tipo %s\n", tipo_stanza[stanza_inizio->tipo] );
+     }
+      else
+
+        if (random>=40 && random<=69)
+        {
+          printf("La stanza iniziale è di tipo %s\n", tipo_stanza[stanza_inizio->tipo] );
+        }
+        else
+
+          if (random>=70 && random<=99)
+          {
+            printf("La stanza iniziale è di tipo %s\n", tipo_stanza[stanza_inizio->tipo] );
+          }
+
+  }
 void stampa_giocatori() //stampa le informazioni relative ad ogni giocatore
 {
-  for (size_t i = 0; i < n_giocatori; i++)
+  for (int i = 0; i < n_giocatori; i++)
   {
-    printf(" nome= %d \n", giocatori[i].nome );
+
+    switch (giocatori[i].nome)
+    {
+      case 0: printf("rosso\n");
+      break;
+      case 1: printf("blu\n");
+      break;
+      case 2: printf("verde\n");
+      break;
+      case 3: printf("giallo\n");
+      break;
+      case 4: printf("bianco\n");
+      break;
+      case 5: printf("nero\n");
+      break;
+      case 6: printf("marrone\n");
+      break;
+      case 7: printf("arancione\n");
+      break;
+      case 8: printf("viola\n");
+      break;
+      case 9: printf("fucsia\n");
+      break;
+    }
   }
+  prima_stanza(); //tutti i giocatori iniziano il gioco in questa stanza iniziale
 }
 void inizia_gioco(){}
