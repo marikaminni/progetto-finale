495f00c543e951489cbf73c6a54bd62b70daf8f0
marikaminni <minni.marika@virgilio.it>
2021-04-30 17:37:55 +0200

    completate tutte le funzioni

--- gamelib.c
+++ gamelib.c
@@ -15,6 +15,18 @@ static int n_giocatori = 0, n_assassinati = 0, n_defenestrati = 0;
 static int max_num_giocatori = 10;
 _Bool debug = false;
 const char *CLEAR_SCREEN_ANSI = "\e[1;1H\e[2J";
+#define GIOCATORE_ROSSO "\x1b[31m"
+#define GIOCATORE_BLU "\x1b[34m"
+#define GIOCATORE_VERDE "\x1b[32m"
+#define GIOCATORE_GIALLO "\x1b[33m"
+#define GIOCATORE_BIANCO "\x1b[36m"
+#define GIOCATORE_NERO "\x1b[30m\x1b[47m"
+#define GIOCATORE_MARRONE "\x1b[36m"
+#define GIOCATORE_ARANCIONE "\x1b[91m"
+#define GIOCATORE_VIOLA "\x1b[35m"
+#define GIOCATORE_FUCSIA "\x1b[95m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+
 void stampa_stato(enum Stato_giocatore stato);
 void stampa_nome(enum Nome_giocatore nome);
 void stampa_stanza(enum Tipo_stanza choice);
@@ -204,7 +216,9 @@ static void chiamata_emergenza(struct Giocatore *altrogiocatore) {
   }
 };
 static void uccidi_astronauta(struct Giocatore *giocatore_corrente) {
+
   int probabilita = 0;
+  n_defenestrati = 0;
   struct Giocatore *altrogiocatore;
   for (int i = 0; i < n_giocatori; i++) {
     altrogiocatore = &giocatori[i];
@@ -230,20 +244,68 @@ static void uccidi_astronauta(struct Giocatore *giocatore_corrente) {
                altrogiocatore->stato == astronauta) {
       probabilita += 20;
     }
-    if ((rand() % 100) + 1 >= probabilita) {
-      giocatore_corrente->stato = defenestrato;
-      stampa_nome(giocatore_corrente->nome);
-      stampa_stato(giocatore_corrente->stato);
-      n_defenestrati++;
-      break;
-    }
   }
+  if ((rand() % 100) + 1 <= probabilita) {
+    giocatore_corrente->stato = defenestrato;
+    stampa_nome(giocatore_corrente->nome);
+    stampa_stato(giocatore_corrente->stato);
+    n_defenestrati++;
+  }
+
   if (n_defenestrati == 0) {
     printf("Nessuno è stato defenestrato\n");
   }
 };
 static void usa_botola(struct Giocatore *giocatore_corrente) {
+
   if (giocatore_corrente->posizione_stanza->descrizione == botola) {
+    struct Stanza *stanza_corrente = lista_stanze;
+    int n_stanze = 0;
+    while (stanza_corrente != NULL) {
+      if (stanza_corrente != giocatore_corrente->posizione_stanza) {
+        n_stanze++;
+      }
+      if (stanza_corrente != giocatore_corrente->posizione_stanza &&
+          stanza_corrente->descrizione == botola) {
+        int randStanza = ((rand() % 100) + 1);
+        if (randStanza >= 50) {
+          break;
+        }
+      }
+      stanza_corrente = stanza_corrente->stanza_precedente;
+    }
+    while (n_stanze > 0 && stanza_corrente == NULL) {
+      stanza_corrente = lista_stanze;
+      while (stanza_corrente != NULL) {
+        if (stanza_corrente != giocatore_corrente->posizione_stanza) {
+          int randStanza = ((rand() % 100) + 1);
+          if (randStanza >= 50) {
+            break;
+          }
+        }
+        stanza_corrente = stanza_corrente->stanza_precedente;
+      }
+    }
+    if (n_stanze == 0) {
+      printf("non esistono altre stanze sui cui spostarsi\n");
+      return;
+    }
+    giocatore_corrente->posizione_stanza = stanza_corrente;
+    stampa_nome(giocatore_corrente->nome);
+    printf(" ha cambiato stanza\n");
+    stampa_stanza(giocatore_corrente->posizione_stanza->descrizione);
+    printf("altri giocatori nella stanza:\n");
+    for (int i = 0; i < n_giocatori; i++) {
+
+      struct Giocatore *altrogiocatore = &giocatori[i];
+      if (altrogiocatore->posizione_stanza ==
+              giocatore_corrente->posizione_stanza &&
+          altrogiocatore->nome != giocatore_corrente->nome) {
+        printf("\t- ");
+        stampa_nome(altrogiocatore->nome);
+        printf("\n");
+      }
+    }
 
   } else
     printf("Per poter utilizzare la funzione usa botola, la stanza deve "
@@ -388,6 +450,7 @@ void gioca(int n_quest) {
       false) // non si può giocare se non si è impostato il gico
   {
     printf("Prima devi impostare il gioco \n");
+    return;
   } else {
     // printf("\033[0;32m");
     printf("\n\t\t-------------------------------------------------"
@@ -403,6 +466,7 @@ void gioca(int n_quest) {
     ordine_giocatori[i] = i;
   }
   int turno = 1;
+  int playing = 1;
   // int turn_counter = 0;
   do {
     struct Giocatore *giocatore_corrente;
@@ -504,10 +568,9 @@ void gioca(int n_quest) {
           uccidi_astronauta(giocatore_corrente);
         } else if (scelta_corretta == 3) {
           chiamata_emergenza(giocatore_corrente);
-        } /*else if(scelta_corretta==4){
-           usa_botola(giocatore_corrente);
-         }*/
-        else if (scelta_corretta == 5) {
+        } else if (scelta_corretta == 4) {
+          usa_botola(giocatore_corrente);
+        } else if (scelta_corretta == 5) {
           sabotaggio(giocatore_corrente);
         }
 
@@ -519,34 +582,65 @@ void gioca(int n_quest) {
       }
       if (quest_da_finire == n_quest) {
         printf("\t\tGli astronauti vincono!\n");
-        termina_gioco();
+        sleep(3);
+        playing = 0;
+        break;
+      }
+      int vivi_astronauti = 0;
+      int vivi_impostori = 0;
+      for (int i = 0; i < n_giocatori; i++) {
+
+        struct Giocatore *altrogiocatore = &giocatori[i];
+        if (altrogiocatore->stato == astronauta) {
+          vivi_astronauti++;
+        } else if (altrogiocatore->stato == impostore) {
+          vivi_impostori++;
+        }
+      }
+      if (vivi_astronauti == 0) {
+
+        printf("\t\tGli impostori vincono!\n");
+        sleep(3);
+        playing = 0;
+        break;
+      }
+      if (vivi_impostori == 0) {
+
+        printf("\t\tGli astronauti vincono!\n");
+        sleep(3);
+        playing = 0;
+        break;
       }
 
       sleep(2);
     }
     turno++;
-    // break;
-  } while (true);
+
+  } while (playing == 1);
+  termina_gioco();
 }
 
 void termina_gioco() {
-  struct Stanza *stanza_corrente = lista_stanze;
-  while (stanza_corrente != NULL) {
-    printf("%p %p %p\n", stanza_corrente, lista_stanze,
-           lista_stanze->stanza_precedente);
-    lista_stanze = lista_stanze->stanza_precedente;
-    free(stanza_corrente);
-    stanza_corrente = lista_stanze;
+  if (gioco_impostato == true) {
+    struct Stanza *stanza_corrente = lista_stanze;
+    while (stanza_corrente != NULL) {
+      // printf("%p %p %p\n", stanza_corrente, lista_stanze,
+      // lista_stanze->stanza_precedente);
+      lista_stanze = lista_stanze->stanza_precedente;
+      free(stanza_corrente);
+      stanza_corrente = lista_stanze;
+    }
+
+    free(giocatori);
+
+    printf("\n\t\t---------------------------------------------------"
+           "------"
+           "-\n");
+    printf(" \t\t                         GAME OVER             \n");
+    printf("\t\t-----------------------------------------------------"
+           "-----\n");
+    gioco_impostato = false;
   }
-
-  free(giocatori);
-
-  printf("\n\t\t---------------------------------------------------"
-         "------"
-         "-\n");
-  printf(" \t\t                         GAME OVER             \n");
-  printf("\t\t-----------------------------------------------------"
-         "-----\n");
 }
 
 void menu() // Definizione della funzione menu
@@ -579,34 +673,35 @@ void stampa_stanza(enum Tipo_stanza choice) // definisco il tipo
 void stampa_nome(enum Nome_giocatore nome) {
   switch (nome) {
   case 0:
-    printf("il giocatore rosso");
+
+    printf("il giocatore %srosso%s", GIOCATORE_ROSSO, ANSI_COLOR_RESET);
     break;
   case 1:
-    printf("il giocatore blu");
+    printf("il giocatore %sblu%s", GIOCATORE_BLU, ANSI_COLOR_RESET);
     break;
   case 2:
-    printf("il giocatore verde");
+    printf("il giocatore %sverde%s", GIOCATORE_VERDE, ANSI_COLOR_RESET);
     break;
   case 3:
-    printf("il giocatore giallo");
+    printf("il giocatore %sgiallo%s", GIOCATORE_GIALLO, ANSI_COLOR_RESET);
     break;
   case 4:
-    printf("il giocatore bianco");
+    printf("il giocatore %sbianco%s", GIOCATORE_BIANCO, ANSI_COLOR_RESET);
     break;
   case 5:
-    printf("il giocatore nero");
+    printf("il giocatore %snero%s", GIOCATORE_NERO, ANSI_COLOR_RESET);
     break;
   case 6:
-    printf("il giocatore marrone");
+    printf("il giocatore %smarrone%s", GIOCATORE_MARRONE, ANSI_COLOR_RESET);
     break;
   case 7:
-    printf("il giocatore arancione");
+    printf("il giocatore %sarancione%s", GIOCATORE_ARANCIONE, ANSI_COLOR_RESET);
     break;
   case 8:
-    printf("il giocatore viola");
+    printf("il giocatore %sviola%s", GIOCATORE_VIOLA, ANSI_COLOR_RESET);
     break;
   case 9:
-    printf("il giocatore fucsia");
+    printf("il giocatore %sfucsia%s", GIOCATORE_FUCSIA, ANSI_COLOR_RESET);
     break;
   }
 }
